9aa6c4aa616a398c8d403ccad190bfc53e2d210a
toxac <derocon@gmail.com>
2026-07-20 15:23:51 +0530

    updated types

--- types/supabase.ts
+++ types/supabase.ts
@@ -791,39 +791,75 @@ export type Database = {
       projects: {
         Row: {
           biz_name: string | null
+          build_data: Json
+          competitive_landscape: Json
+          compliance_checklist: Json
           created_at: string
+          current_mission: string
           discovery_metrics: Json
           financial_blueprint: Json
           five_word_hook: string | null
           id: string
           infrastructure_nodes: Json
           is_active: boolean
+          launch_data: Json
+          operations_data: Json
+          review_data: Json
+          solution_design: Json
+          status: string
+          tagline: string | null
           updated_at: string
           user_id: string
+          validation_data: Json
+          viability_check: Json
         }
         Insert: {
           biz_name?: string | null
+          build_data?: Json
+          competitive_landscape?: Json
+          compliance_checklist?: Json
           created_at?: string
+          current_mission?: string
           discovery_metrics?: Json
           financial_blueprint?: Json
           five_word_hook?: string | null
           id?: string
           infrastructure_nodes?: Json
           is_active?: boolean
+          launch_data?: Json
+          operations_data?: Json
+          review_data?: Json
+          solution_design?: Json
+          status?: string
+          tagline?: string | null
           updated_at?: string
           user_id?: string
+          validation_data?: Json
+          viability_check?: Json
         }
         Update: {
           biz_name?: string | null
+          build_data?: Json
+          competitive_landscape?: Json
+          compliance_checklist?: Json
           created_at?: string
+          current_mission?: string
           discovery_metrics?: Json
           financial_blueprint?: Json
           five_word_hook?: string | null
           id?: string
           infrastructure_nodes?: Json
           is_active?: boolean
+          launch_data?: Json
+          operations_data?: Json
+          review_data?: Json
+          solution_design?: Json
+          status?: string
+          tagline?: string | null
           updated_at?: string
           user_id?: string
+          validation_data?: Json
+          viability_check?: Json
         }
         Relationships: [
           {
